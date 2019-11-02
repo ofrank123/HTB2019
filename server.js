a8ecc9138831961724a6f84f43601b02c5682b2b
of9 <of9@st-andrews.ac.uk>
2019-11-02 18:37:20 +0000

    chaning port

--- server.js
+++ server.js
@@ -94,4 +94,4 @@ app.get('/', (req, res) => {
     });
 });
 
-app.listen(3000, () => console.log('app listening on port 3000!'));
+app.listen(8080, () => console.log('app listening on port 8080!'));
